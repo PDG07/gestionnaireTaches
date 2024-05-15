ab410491925e1157aeb82d00281ceed65975f1a4
Nabil Hasnaoui <nabil.hasnaoui.it@gmail.com>
2024-05-15 12:00:09 -0400

    initial commit

---
+++ gestionnaireTaches/src/main/java/ved/firstproject/gestionnairetaches/GestionnaireTachesApplication.java
@@ -0,0 +1,13 @@
+package ved.firstproject.gestionnairetaches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionnaireTachesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionnaireTachesApplication.class, args);
+    }
+
+}
