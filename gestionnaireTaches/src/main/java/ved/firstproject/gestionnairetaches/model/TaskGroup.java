29e995e81722529d801122537dd2b8daf14980aa
Nabil Hasnaoui <nabil.hasnaoui.it@gmail.com>
2024-05-19 15:39:27 -0400

    TaskGroup sans Sets

--- gestionnaireTaches/src/main/java/ved/firstproject/gestionnairetaches/model/TaskGroup.java
+++ gestionnaireTaches/src/main/java/ved/firstproject/gestionnairetaches/model/TaskGroup.java
@@ -1,14 +1,27 @@
 package ved.firstproject.gestionnairetaches.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class TaskGroup {
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
     private Long id;
-    @Column(name = "name")
-    private String name;
+    @Column(name = "title")
+    private String title;
+
+    public TaskGroup(String title) {
+        this.title = title;
+    }
+
     //Set of users
     //Set of tasks
+    //Set of tasksGroupHistory
 }
