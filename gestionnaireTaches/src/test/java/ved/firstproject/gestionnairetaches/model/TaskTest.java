eed677c984ca9773b899c1bae8c01778e1d55dd6
Nabil Hasnaoui <nabil.hasnaoui.it@gmail.com>
2024-05-19 17:23:31 -0400

    TastTest Empty Created

---
+++ gestionnaireTaches/src/test/java/ved/firstproject/gestionnairetaches/model/TaskTest.java
@@ -0,0 +1,17 @@
+package ved.firstproject.gestionnairetaches.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void completeTask() {
+    }
+}
