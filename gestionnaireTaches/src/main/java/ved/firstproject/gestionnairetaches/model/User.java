504517cdd559b4703c665db921ce5cb8b4f240c3
Nabil Hasnaoui <nabil.hasnaoui.it@gmail.com>
2024-05-20 10:12:14 -0400

    Constructor tasksList fix

--- gestionnaireTaches/src/main/java/ved/firstproject/gestionnairetaches/model/User.java
+++ gestionnaireTaches/src/main/java/ved/firstproject/gestionnairetaches/model/User.java
@@ -38,6 +38,7 @@ public class User {
         this.id = id;
         this.username = username;
         this.password = password;
+        this.tasks = tasks;
     }
 
     public void addTask(Task task) {
