d7e5ff3d168a1fc1413ff721c7a5a4065f91c5c4
Nabil Hasnaoui <nabil.hasnaoui.it@gmail.com>
2024-07-09 11:31:31 -0400

    Updated Config package

--- gestionnaireTaches/src/main/java/ved/firstproject/gestionnairetaches/config/SecurityConfig.java
+++ gestionnaireTaches/src/main/java/ved/firstproject/gestionnairetaches/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,18 +22,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/api/signup",
-                                "/api/createtask",
-                                "/api/tasks",
-                                "/api/updatetask",
-                                "/api/completetask/**",
-                                "/api/completedtasks",
-                                "/api/tasks/filter",
-                                "/api/group/create",
-                                "/api/group/addTask",
-                                "/api/group/findGroupById")
-                )
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/signup",
                                 "/api/createtask",
--- gestionnaireTaches/src/main/java/ved/firstproject/gestionnairetaches/config/WebConfig.java
+++ gestionnaireTaches/src/main/java/ved/firstproject/gestionnairetaches/config/WebConfig.java
@@ -1,5 +1,4 @@
-/*package ved.firstproject.gestionnairetaches.config;
-import org.springframework.context.annotation.Bean;
+package ved.firstproject.gestionnairetaches.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,7 +8,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
-*/
